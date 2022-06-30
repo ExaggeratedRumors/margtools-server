ae6cde1c7d0950389377b9f7cf9b1989381dc68a
ExaggeratedRumors <exaggeratedrumors@gmail.com>
2022-06-30 13:12:40 +0200

    Initial commit

---
+++ src/main/kotlin/com/margtools/Application.kt
@@ -0,0 +1,12 @@
+package com.margtools
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.margtools.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureTemplating()
+    }.start(wait = true)
+}
